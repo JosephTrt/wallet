5d39eeabd80a7302abb7109efa84a581aa414341
Jose Trepat <trepatj@gmail.com>
2022-10-31 19:29:07 -0300

    Generando migraciones y cambios en la configuracion de la base

--- backend/backend/settings/local.py
+++ backend/backend/settings/local.py
@@ -11,18 +11,18 @@ ALLOWED_HOSTS = []
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'wallet',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost', #127.0.0.1
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'wallet',
+    #     'USER': 'root',
+    #     'PASSWORD': '',
+    #     'HOST': 'localhost', #127.0.0.1
+    #     'PORT': '3306'
+    # }
 }
 
 
